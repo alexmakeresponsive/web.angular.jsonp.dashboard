c62ef6510e147a58c7be5c43c007c62d121f1f74
amr <alexmakeresponsive@gmail.com>
2020-03-19 11:25:46 +0600

    app: update save data

--- client/main/app/src/app/dashboard/routed/page/home/component.ts
+++ client/main/app/src/app/dashboard/routed/page/home/component.ts
@@ -15,38 +15,7 @@ export class PageDashboardHomeComponent implements OnInit{
   ) {
   }
 
-  private getData(tokenValue, idUser) {
-    var script1     = <HTMLScriptElement>document.createElement('SCRIPT');
-    var script2     = <HTMLScriptElement>document.createElement('SCRIPT');
-
-    script1.src = "https://api.vk.com/method/users.get?user_ids="+idUser+"&fields=bdate,sex,photo_200&access_token="+tokenValue+"&v=5.103&callback=callbackFuncSelf";
-    script2.src = "https://api.vk.com/method/friends.get?user_ids="+idUser+"&fields=bdate,sex,photo_200&order=random&count=5&access_token="+tokenValue+"&v=5.103&callback=callbackFuncFriend";
-
-    document.getElementsByTagName("head")[0].appendChild(script1);
-    document.getElementsByTagName("head")[0].appendChild(script2);
-  }
-
-
   ngOnInit(): void {
 
-    const token = this.authGuard.vk.token;
-    const id = this.authGuard.vk.id;
-
-    if (token) {
-      this.getData(token, id);
-    }
-  }
-
-
-  @HostListener('window:dataSelf', ['$event'])
-  dataSelfListener(event) {
-    console.log(event);
-    this.storageData.vk.data.self = event.detail
-  }
-
-  @HostListener('window:dataFriends', ['$event'])
-  dataFriendsListener(event) {
-    console.log(event);
-    this.storageData.vk.data.friends = event.detail
   }
 }
--- client/main/app/src/app/dashboard/routed/page/save/component.ts
+++ client/main/app/src/app/dashboard/routed/page/save/component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit} from '@angular/core';
+import {Component, AfterViewInit, HostListener} from '@angular/core';
 import {Router} from "@angular/router";
 
 import {StorageData} from "@CommonServiceStorageModule/storage.data";
@@ -10,6 +10,8 @@ import {AuthGuard} from "@CommonServiceAuthModule/auth.guard";
   styleUrls: ['./component.styl']
 })
 export class PageDashboardSaveComponent implements AfterViewInit{
+
+  eventListenerCount = 0;
 
   constructor(
     private storageData:StorageData,
@@ -36,12 +38,40 @@ export class PageDashboardSaveComponent implements AfterViewInit{
     return l2;
   }
 
+  private getData(tokenValue, idUser) {
+    var script1     = <HTMLScriptElement>document.createElement('SCRIPT');
+    var script2     = <HTMLScriptElement>document.createElement('SCRIPT');
+
+    script1.src = "https://api.vk.com/method/users.get?user_ids="+idUser+"&fields=bdate,sex,photo_200&access_token="+tokenValue+"&v=5.103&callback=callbackFuncSelf";
+    script2.src = "https://api.vk.com/method/friends.get?user_ids="+idUser+"&fields=bdate,sex,photo_200&order=random&count=5&access_token="+tokenValue+"&v=5.103&callback=callbackFuncFriend";
+
+    document.getElementsByTagName("head")[0].appendChild(script1);
+    document.getElementsByTagName("head")[0].appendChild(script2);
+  }
+
+  private redirect() {
+    const urlLastVisited = JSON.parse(window.localStorage.router).url.lastVisited;
+
+    this.eventListenerCount = 0;
+
+    if (this.authGuard.access) {
+      this.router.navigateByUrl(urlLastVisited);
+    } else {
+      this.router.navigateByUrl('login');
+    }
+  }
+
+  private checkEventListenerCount() {
+    this.eventListenerCount += 1;
+
+    if(this.eventListenerCount === 2) {
+      this.redirect();
+    }
+  }
+
   ngAfterViewInit(): void {
     const id           = this.getParameterByName('user_id', window.location.href);
     const token        = this.getHash(window.location.hash);
-
-    const urlLastVisited = JSON.parse(window.localStorage.router).url.lastVisited;
-
 
     if (token) {
       this.authGuard.access = true;
@@ -50,12 +80,22 @@ export class PageDashboardSaveComponent implements AfterViewInit{
       this.authGuard.vk.token = token;
 
       window.localStorage.auth = JSON.stringify({logout: false});
-    }
 
-    if (this.authGuard.access) {
-      this.router.navigateByUrl(urlLastVisited);
-    } else {
-      this.router.navigateByUrl('login');
+      this.getData(token, id);
     }
+  }
+
+  @HostListener('window:dataSelf', ['$event'])
+  dataSelfListener(event) {
+    console.log(event);
+    this.storageData.vk.data.self = event.detail;
+    this.checkEventListenerCount();
+  }
+
+  @HostListener('window:dataFriends', ['$event'])
+  dataFriendsListener(event) {
+    console.log(event);
+    this.storageData.vk.data.friends = event.detail;
+    this.checkEventListenerCount();
   }
 }
