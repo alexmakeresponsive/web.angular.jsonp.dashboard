5f4d63067532b045e21a9f57ad1fe5b6a70c5158
amr <alexmakeresponsive@gmail.com>
2020-03-19 11:31:40 +0600

    app: check save data on profile page

--- client/main/app/src/app/dashboard/routed/page/profile/component.ts
+++ client/main/app/src/app/dashboard/routed/page/profile/component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit } from '@angular/core';
+
+import {StorageData} from "@CommonServiceStorageModule/storage.data";
 
 @Component({
   selector: 'page-main-home',
   templateUrl: './component.html',
   styleUrls: ['./component.styl']
 })
-export class PageDashboardProfileComponent {
-  constructor() {
+export class PageDashboardProfileComponent implements OnInit{
+  constructor(
+    private storageData:StorageData,
+  ) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.storageData);
   }
 }
